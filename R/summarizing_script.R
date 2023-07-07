f694ba56078c55af48e15df77e54805e18013348
Will Cornwell <w.cornwell@unsw.edu.au>
2023-07-07 11:54:11 +1000

    current summary document

--- R/summarizing_script.R
+++ R/summarizing_script.R
@@ -8,14 +8,24 @@ indo_non_aus<-filter(indo,!grepl("Aus",Country))
 indo_non_aus<-filter(indo_non_aus,!grepl("Sin",Country)) #there seem to be problems with the Singapore data
 ww$indo2<-ww$genus %in% indo_non_aus$Genus
 
+ala <- read.csv("ala_output.csv")
+ww<-left_join(ww,ala)
 ww %>%
-  select(nz,nc,ng,wcvp,indo2)->tst
+  mutate(ala=outside_aus_num>5&outside_aus_prop>0.6)->ww
+
+ww %>%
+  select(nz,nc,ng,wcvp,indo2,ala)->tst
 ww$rowsums<-rowSums(tst)
+
+
+
 
 ww$current_knowledge<-case_when(ww$rowsums>=2 ~ "non-endemic",
           ww$rowsums==1 ~ "unclear",
           ww$rowsums==0 ~ "endemic"
           )
+
+
 
 table(ww$current_knowledge)
 write_csv(ww,"intermediate_data/five_sources_summary.csv")
