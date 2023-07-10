7f6d6b9e8adae4cb61216cf0f684843e3297d087
Will Cornwell <w.cornwell@unsw.edu.au>
2023-07-11 09:23:28 +1000

    better way to use ALA

--- R/summarizing_script.R
+++ R/summarizing_script.R
@@ -8,24 +8,33 @@ indo_non_aus<-filter(indo,!grepl("Aus",Country))
 indo_non_aus<-filter(indo_non_aus,!grepl("Sin",Country)) #there seem to be problems with the Singapore data
 ww$indo2<-ww$genus %in% indo_non_aus$Genus
 
-ala <- read.csv("ala_output.csv")
-ww<-left_join(ww,ala)
+ala<-read_csv("all_ala.csv")
+  group_by(ala,genus) %>%
+    summarise(prop_aus=(sum(countryCode=="AU",na.rm=T)/sum(!is.na(countryCode))),
+              n=sum(!is.na(countryCode)))->sum_ala
+
+ww<-left_join(ww,sum_ala)
 ww %>%
-  mutate(ala=outside_aus_num>5&outside_aus_prop>0.6)->ww
+  mutate(ala=prop_aus<0.99)->ww
 
 ww %>%
   select(nz,nc,ng,wcvp,indo2,ala)->tst
 ww$rowsums<-rowSums(tst)
-
-
-
 
 ww$current_knowledge<-case_when(ww$rowsums>=2 ~ "non-endemic",
           ww$rowsums==1 ~ "unclear",
           ww$rowsums==0 ~ "endemic"
           )
 
-
-
 table(ww$current_knowledge)
 write_csv(ww,"intermediate_data/five_sources_summary.csv")
+
+gen_int<-read_csv("ala_all_genera_of_interest.csv")
+
+only_int<-filter(ww,current_knowledge %in% c("unclear","endemic"))
+
+filter(gen_int,genus %in% only_int$genus) %>%
+  left_join(select(only_int,genus,current_knowledge)) %>%
+  write_csv("locations_endemic_genera.csv")
+  
+
